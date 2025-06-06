11500897c866cdb117110c31bf892becf6bf7b1f
Igor Marques <52253601+igormcouto@users.noreply.github.com>
2025-06-06 15:30:39 +0000

    Subindo ajustes configuração nome projeto

--- Igor_Angular_v1.0.0/igor/src/app/app.component.spec.ts
+++ Igor_Angular_v1.0.0/igor/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'natia-ng' title`, () => {
+  it(`should have the 'igor-ng' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('natia-ng');
+    expect(app.title).toEqual('igor-ng');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, natia-ng');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, igor-ng');
   });
 });
