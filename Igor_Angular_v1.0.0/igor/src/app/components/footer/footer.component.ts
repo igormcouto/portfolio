8010f11d75a5ff141e91cbe081de7aab28a6aa5d
Igor Marques <52253601+igormcouto@users.noreply.github.com>
2025-06-06 17:13:05 +0000

    ajustes link redirect

--- Igor_Angular_v1.0.0/igor/src/app/components/footer/footer.component.ts
+++ Igor_Angular_v1.0.0/igor/src/app/components/footer/footer.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
   year = new Date().getFullYear()
+
+  openLink(link: string): void {
+    window.open(link, '_blank');
+  }
 }
